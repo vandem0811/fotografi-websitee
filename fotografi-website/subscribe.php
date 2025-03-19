18ccfea91a02a387d3195310cee51acfd44f0f9b
vandem0811 <r1noingff@gmail.com>
2025-03-20 07:53:59 +0800

    Delete fotografi-website directory

--- fotografi-website/subscribe.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST["email"];
-    file_put_contents("subscribers.txt", $email . PHP_EOL, FILE_APPEND);
-    echo "Thank you for subscribing!";
-}
-?>
